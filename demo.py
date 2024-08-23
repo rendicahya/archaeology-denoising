c2273b2a5d27d910543949651bc5780e71530336
Randy Cahya Wihandika <rendicahya@gmail.com>
2024-08-23 19:31:28 +0900

    Added batch demo

--- demo.py
+++ demo.py
@@ -1,3 +1,4 @@
+import multiprocessing
 from pathlib import Path
 
 import click
@@ -5,6 +6,7 @@ import torch
 from config import settings as conf
 from models import Autoencoder, ViT
 from PIL import Image
+from torch.utils.data import DataLoader
 from torchvision import transforms
 
 
@@ -27,7 +29,6 @@ def test(model, device, test_loader, criterion):
 
 def save_image(tensor, path):
     image = tensor.cpu().clone().detach()
-    image = image.squeeze(0).squeeze(0)
     image *= 255
     image = image.to(torch.uint8)
     image = Image.fromarray(image.numpy(), mode="L")
@@ -35,26 +36,27 @@ def save_image(tensor, path):
     image.save(path)
 
 
-@cli.command()
+@click.command()
+@click.option(
+    "-c",
+    "--checkpoint",
+    type=click.Path(
+        exists=True,
+        file_okay=True,
+        dir_okay=False,
+        path_type=Path,
+    ),
+    required=True,
+    help="Path to the checkpoint",
+)
 @click.option(
     "-i",
-    "--image",
-    type=click.Path(exists=True),
+    "--input",
+    type=click.Path(exists=True, file_okay=True, dir_okay=True, path_type=Path),
     required=True,
-    file_okay=True,
-    dir_okay=True,
-    help="Path to the image",
+    help="Path to the input image",
 )
-@click.option(
-    "-m",
-    "--model",
-    type=click.Path(exists=True),
-    required=True,
-    file_okay=True,
-    dir_okay=False,
-    help="Path to the model",
-)
-def main():
+def main(checkpoint, input):
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
     transform = transforms.Compose(
         [
@@ -67,19 +69,52 @@ def main():
 
     # model = Autoencoder().to(device)
     model = ViT(img_size=conf.image_size, in_channels=1, num_classes=1).to(device)
+    model.load_state_dict(torch.load(checkpoint, map_location=device))
+    Path("output").mkdir(exist_ok=True)
 
-    model_path = "checkpoints/ViT-600-20240822_182409.pth"
-    image_path = Path("../../datasets/arch_denoising/noise/image-002.jpg")
-    image = Image.open(image_path).convert("L")
-    image = transform(image)
-    image = image.unsqueeze(0).to(device)
+    if input.is_file():
+        image = Image.open(input).convert("L")
+        image = transform(image)
+        image = image.unsqueeze(0).to(device)
 
-    model.load_state_dict(torch.load(model_path, map_location=device))
+        with torch.no_grad():
+            output = model(image)
 
-    with torch.no_grad():
-        output = model(image)
+        output = output.squeeze(0).squeeze(0)
+        save_image(output, f"output/{model._get_name()}-{input.name}")
+    else:
+        dataset = Dataset(input, transform)
+        batch_size = conf.batch_size
+        n_cpu = multiprocessing.cpu_count()
+        loader = DataLoader(dataset, batch_size, pin_memory=True, num_workers=n_cpu)
+        n = 0
 
-    save_image(output, f"output/{model._get_name()}-{image_path.name}")
+        with torch.no_grad():
+            for batch in loader:
+                batch = batch.to(device)
+                output = model(batch)
+
+                for image in output:
+                    image = image.squeeze(0)
+
+                    save_image(image, f"output/{model._get_name()}-{n}.jpg")
+                    n += 1
+
+
+class Dataset(torch.utils.data.Dataset):
+    def __init__(self, path, transform):
+        self.paths = [f for f in Path(path).iterdir() if f.is_file()]
+        self.transform = transform
+
+    def __len__(self):
+        return len(self.paths)
+
+    def __getitem__(self, idx):
+        path = self.paths[idx]
+        image = Image.open(path).convert("L")
+        image = self.transform(image)
+
+        return image
 
 
 if __name__ == "__main__":
