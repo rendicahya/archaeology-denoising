92900606c6556af8af7b3d0fcc64bccb0ed40837
Randy Cahya Wihandika <rendicahya@gmail.com>
2024-08-26 17:41:24 +0900

    Minor

--- utils/config_utils.py
+++ utils/config_utils.py
@@ -5,7 +5,7 @@ from pathlib import Path
 def load_config(config_path):
     spec = importlib.util.spec_from_file_location("config", config_path)
     config = importlib.util.module_from_spec(spec)
-    config.config_name = Path(config_path).stem
+    config.config_name = config_path.stem
 
     spec.loader.exec_module(config)
 
