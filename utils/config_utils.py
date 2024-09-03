629a6c3583488013123815ad51a9cce42e77eab6
Randy Cahya Wihandika <rendicahya@gmail.com>
2024-09-03 11:25:33 +0900

    Casted to pathlib Path

--- utils/config_utils.py
+++ utils/config_utils.py
@@ -3,6 +3,7 @@ from pathlib import Path
 
 
 def load_config(config_path):
+    config_path = Path(config_path)
     spec = importlib.util.spec_from_file_location("config", config_path)
     config = importlib.util.module_from_spec(spec)
     config.config_name = config_path.stem
